d6e8dbf319a6f97b553e4b694a6b84adfa6a56b8
Maria Vitória <132756495+V8tor81@users.noreply.github.com>
2025-06-18 11:29:52 -0300

    AJEITAR ERROS

--- projeto_flask_aula/app/routes.py
+++ projeto_flask_aula/app/routes.py
@@ -2,6 +2,8 @@ from app import app
 from flask import render_template
 from app.forms.login_form import LoginForm
 from app.controllers.authenticationController import AuthenticationController
+from app.models  import Usuario
+from app import db
 
 
 @app.route("/")
@@ -12,6 +14,9 @@ def home():
 @app.route("/sobre")
 def sobre():
     return "Página Sobre"
+@app.route("/contato")
+def contato():
+    return "Página de Contatos"
 
 
 @app.route("/login", methods=['GET', 'POST'])
@@ -20,3 +25,15 @@ def login():
     if formulario.validate_on_submit():
         return AuthenticationController.login(formulario)
     return render_template('login.html', title='Login', form = formulario)
+
+@app.route("/dados", methods= ['GET', 'POST'])
+def dados():
+    usuario = Usuario(username='leosilva', email='leo@leo.com')
+    db.session.add(usuario)
+    db.session.commit()
+    return render_template("index.html", usuario = None, usuario_logado = False)
+
+
+@app.route("/inserir", methods=['GET', 'POST'])
+def inserir():
+    if request.methods == 'POST'
